6de8079e6fb2f3a311f472d6896ce4bfb45d1cc6
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-04-24 19:04:20 +0530

    code added

--- Code/Chinese Remainder Theorem/CodeSpeedy.java
+++ Code/Chinese Remainder Theorem/CodeSpeedy.java
@@ -1,23 +1,18 @@
 import java.util.Scanner;
-
 public class CodeSpeedy {
     static int chineseRemainderTheorem(int[] a, int[] m, int n, int product) {
         int result = 0;
         int[] M = new int[n]; // Store M values
-
         for (int i = 0; i < n; i++) {
             M[i] = product / m[i];
         }
-
         for (int i = 0; i < n; i++) {
             int y;
             for (y = 0; (M[i] * y) % m[i] != 1; y++);
             result = (result + a[i] * M[i] * y) % product;
         }
-
         return result;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of congruence relations: ");
@@ -36,7 +31,7 @@ public class CodeSpeedy {
             m[i] = scanner.nextInt();
             product *= m[i];
         }
-
+        
         System.out.println("The solution is " + chineseRemainderTheorem(a, m, size, product));
         scanner.close();
     }
